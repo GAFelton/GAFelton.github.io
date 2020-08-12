6450164c0d2f5f75b4f74ddece4a33c88943abd4
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-11 18:06:26 -0700

    Added script.js, moved document.foundation

---
+++ script.js
@@ -0,0 +1,19 @@
+$(document).foundation();
+
+$(document).ready(
+    
+    // $(document).click(function iconflip() {
+    //     var icon = $(".pulloutMenuButton");
+    //     var open = false;
+
+    //     if ($("#my-info").hasClass("is-open")) {
+    //         icon.removeClass('open');
+    //         open = !open;
+
+    //     }
+    //     else if ($("#my-info").hasClass("is-closed")) {
+    //         icon.addClass('open'); 
+    //         open = !open;
+    //     }
+    // })
+);
