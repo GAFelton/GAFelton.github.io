0bcbd016a471f7cdd4cc1945458127f574c5a85f
George F <64555318+GAFelton@users.noreply.github.com>
2020-08-21 14:34:20 -0700

    Removed commented-out code.

--- script.js
+++ script.js
@@ -1,27 +1,7 @@
 $(document).foundation();
 
-// document.cookie = 'cookie2=value2; SameSite=None; Secure';
-
 $(document).ready(
 
     $("[data-menu-underline-from-center] a").addClass("underline-from-center")
 
-    
-
-
-    
-    // $(document).click(function iconflip() {
-    //     var icon = $(".pulloutMenuButton");
-    //     var open = false;
-
-    //     if ($("#my-info").hasClass("is-open")) {
-    //         icon.removeClass('open');
-    //         open = !open;
-
-    //     }
-    //     else if ($("#my-info").hasClass("is-closed")) {
-    //         icon.addClass('open'); 
-    //         open = !open;
-    //     }
-    // })
 );
